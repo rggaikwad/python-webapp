6bdff0b116dde077c233c40e1474c20f93c4aac9
usertwo <beniwal29@outlook.com>
2024-02-07 06:08:24 +0000

    updated

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,5 +1,4 @@
 pipeline {
-  agent { docker { image 'python:3.9.4' } }
   stages {
     stage('build') {
       steps {
--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def hello_world():
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
     # show the user profile for that user
-    return 'Why Hello %s!\n' % username
+    return 'Hello %s!\n' % username
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')  # open for everyone
